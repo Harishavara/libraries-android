27396464d98ad47732a35477bc89ab4c4981d51e
Harisha Vara <Harisha.v102@gmail.com>
2019-02-13 16:14:46 +0530

    Initial commit

---
+++ Progressloader/loadlibrary/src/main/java/eeesoft/com/loadlibrary/ShowDialog.java
@@ -0,0 +1,9 @@
+package eeesoft.com.loadlibrary;
+
+public class ShowDialog {
+
+    private static final String TAG = ShowDialog.class.getSimpleName();
+    public static void dialog(String message){
+
+    }
+}
